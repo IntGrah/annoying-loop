1ccd0b5870ae60b7c132a6b881e45befc1e3b634
Jeremy Chen <78419282+RedGoldBelt@users.noreply.github.com>
2023-03-23 22:09:24 +0000

    Update filler.js

--- filler.js
+++ filler.js
@@ -1,5 +1,5 @@
 function filler1() {
-    modulate(); noise(5); middle(); bass(false); progress(4);
+    modulate(); offbeat(); noise(5); middle(); bass(false); progress(4);
     offbeat(); noise(3); middle(); bass(); progress(4);
     modulate(); offbeat(); noise(6); middle(); bass(); progress(4);
     modulate(); offbeat(); noise(6); middle(); bass(); progress(4);
@@ -22,7 +22,7 @@ function filler3() {
 function filler4() {
     modulate(); noise(5); middle(); bass(false); progress(4);
     offbeat(); noise(3); middle(); bass(); progress(4);
-    modulate(); noise(6); middle(); bass(false); progress(4);
+    modulate(); offbeat(); noise(6); middle(); bass(false); progress(4);
     offbeat(); noise(6); middle(); bass(); progress(4);
 }
 
@@ -30,12 +30,12 @@ function filler5() {
     modulate(); noise(5); bass(); progress(4);
     modulate(); rain1(); middle(); bass(); progress(4);
     modulate(); noise(5); bass(); progress(4);
-    modulate(); bass(); middle(); progress(4);
+    modulate(); offbeat(); bass(); middle(); progress(4);
 }
 
 function filler6() {
     modulate(); noise(5); middle(); bass(); progress(4);
-    modulate(); rain1(); middle(); bass(); progress(4);
+    modulate(); offbeat(); rain1(); middle(); bass(); progress(4);
     modulate(); noise(5); middle(); bass(); progress(4);
-    modulate(); rain1(); middle(); bass(); progress(4);
+    modulate(); offbeat(); rain1(); middle(); bass(); progress(4);
 }
