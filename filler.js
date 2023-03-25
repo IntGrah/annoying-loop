1b0665b6717d4be2a6a1561cbea81200b0e057c7
RedGoldBelt <78419282+RedGoldBelt@users.noreply.github.com>
2023-03-25 22:47:33 +0000

    Update filler.js

--- filler.js
+++ filler.js
@@ -44,16 +44,16 @@ const arpeggiaic = [-2, 2];
 const scalic = [-2, -1, -1, -1, -1, 1, 1, 1, 1, 2];
 
 const Scale = new Map();
-Scale.set(Chord.Major7th, [7, 9, 11, 12, 14, 16, 17, 19, 21, 23]);
-Scale.set(Chord.Minor9th, [7, 8, 10, 12, 14, 15, 17, 19, 20, 22]);
-Scale.set(Chord.Major9th, [7, 9, 11, 12, 14, 16, 18, 19, 21, 23]);
-Scale.set(Chord.Dominant9th, [7, 9, 10, 12, 14, 16, 17, 19, 21, 22]);
-Scale.set(Chord.Minor11th, [7, 9, 10, 12, 14, 16, 17, 19, 21, 22]);
-Scale.set(Chord.Major11th, [7, 9, 11, 12, 14, 16, 18, 19, 21, 23]);
+Scale.set(Chord.Major7th, [7, 9, 11, 12, 14, 16, 19, 21, 23]);
+Scale.set(Chord.Minor9th, [7, 9, 10, 12, 14, 15, 19, 21, 22]);
+Scale.set(Chord.Major9th, [7, 9, 11, 12, 14, 16, 19, 21, 23]);
+Scale.set(Chord.Dominant9th, [7, 9, 10, 12, 14, 16, 19, 21, 22]);
+Scale.set(Chord.Minor11th, [7, 9, 10, 12, 14, 17, 19, 21, 22]);
+Scale.set(Chord.Major11th, [7, 9, 11, 12, 14, 16, 18, 19, 21]);
 
 function improv() {
     const rhythm = randEl([bebop, syncopation]);
-    let index = randInt(0, 10);
+    let index = randInt(0, 9);
     const move = contour => index = randEl(contour.map(n => n + index).filter(n => 0 <= n && n < 10));
     for (let n = 0; n < 4; ++n) {
         if (coin()) tonic = randTonic();
